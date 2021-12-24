0a025039294a4683e750cd4ca8a590f409028271
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2021-12-24 13:52:23 +0530

    implement deploy script for etheremum networks

---
+++ deploy.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider({
+  privateKeys: [process.env.PRIVATE_KEY],
+  providerOrUrl: process.env.INFURA_API_URL,
+});
+
+const web3 = new Web3(provider);
+let initialMessage = "Hello World.";
+
+//deploy contract
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log("Contract is being deployed from", accounts[0]);
+
+  const newContractInstance = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: [initialMessage],
+    })
+    .send({ from: accounts[0] });
+
+  console.log("Contract deployed to", newContractInstance.options.address);
+  provider.engine.stop();
+};
+
+deploy();
